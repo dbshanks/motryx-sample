09224403a2150d249412026f6ac381cbb72fc372
Derek Shanks <dbshanks75@hotmail.com>
2020-10-12 04:09:37 -0300

    Added a UI perk that allows user to click the table row to go to wildcard page

--- pages/loghistory/index.js
+++ pages/loghistory/index.js
@@ -1,24 +1,42 @@
 import React from 'react';
 import Link from 'next/link';
 import { Row, Column } from '@Layout/Grid';
+import styles from './loghistory.module.scss';
+import { useRouter } from 'next/router';
 
 const LogHistory = ({ data }) => {
+  const router = useRouter();
+
   return (
     <div>
       <Row>
         <Column col={12}>
           <table>
+            <thead>
+              <tr>
+                <th>Vial ID</th>
+                <th>Zone From</th>
+                <th>Zone To</th>
+                <th>User</th>
+                <th>Description</th>
+              </tr>
+            </thead>
             <tbody>
               {data.map((item) => {
                 return (
-                  <tr key={item._id}>
+                  <tr
+                    key={item._id}
+                    onClick={() => router.push(`/${item._id}`)}
+                    className={styles.rowClicks}>
                     <td>{item.vial_id}</td>
                     <td>{item.zone_from}</td>
                     <td>{item.zone_to}</td>
                     <td>{item.user}</td>
                     <td>{item.description}</td>
                     <td>
-                      <Link href={`/${item._id}`}>Edit</Link>
+                      <Link href={`/${item._id}`}>
+                        <a>Edit</a>
+                      </Link>
                     </td>
                   </tr>
                 );
