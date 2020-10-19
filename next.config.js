418a3aea54414386f889bea4851b6be04edfedfb
Derek Shanks <dbshanks75@hotmail.com>
2020-10-18 22:32:53 -0300

    _

--- next.config.js
+++ next.config.js
@@ -4,4 +4,8 @@ module.exports = {
         @import './scss/partials/_vars.scss';
     `,
   },
+  env: {
+    MONGO_URI:
+      'mongodb+srv://dbshanks:sandbox9@cluster0.b8cbg.mongodb.net/motryx?retryWrites=true&w=majority',
+  },
 };
